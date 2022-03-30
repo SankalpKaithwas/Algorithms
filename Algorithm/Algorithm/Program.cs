70bfaa89d124adfd833b899218db6ab80f7fcb67
unknown <kaithwas.697@gmail.com>
2022-03-30 17:49:04 +0530

    Added feature to find number of permutations for a string

---
+++ Algorithm/Algorithm/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chhose 1: for String permutations");
+            int options = int.Parse(Console.ReadLine());
+
+            switch (options)
+            {
+                case 1:
+                    Console.WriteLine("Enter a string");
+                    string word = Console.ReadLine();
+                    Console.WriteLine("Recursive ->");
+                    StringPermutation.RecursivePermutation(word, 0, word.Length - 1);
+                    Console.WriteLine("Iterarive ->");
+                    StringPermutation.IterativeFindPermutation("", word);
+                    break;
+                default:
+                    Console.WriteLine("Choose valid");
+                    break;
+            }
+        }
+    }
+}
---
+++ Algorithm/Algorithm/StringPermutation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace Algorithm
+{
+    internal class StringPermutation
+    {
+        public static void IterativeFindPermutation(string word, string remaining)
+        {
+            if (remaining == null)
+            {
+                return;
+            }
+            else if (remaining.Length == 0)
+            {
+                Console.WriteLine(word);
+            }
+            else
+            {
+                for (int i = 0; i < remaining.Length; i++)
+                {
+                    string newCandidate = word + remaining[i];
+                    string newRemaining = remaining.Substring(0, i) + remaining.Substring(i + 1);
+                    IterativeFindPermutation(newCandidate, newRemaining);
+                }
+            }
+        }
+        public static void RecursivePermutation(string str, int l, int r)
+        {
+            if (l == r)
+            {
+                Console.WriteLine(str);
+            }
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = swap(str, l, i);
+                    RecursivePermutation(str, l + 1, r);
+                    str = swap(str, l, i);
+                }
+            }
+        }
+        public static string swap(string a, int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+    }
+}

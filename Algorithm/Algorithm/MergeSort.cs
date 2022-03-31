7477c9d7cd107c06fea435562187110def6443a1
unknown <kaithwas.697@gmail.com>
2022-03-31 17:45:11 +0530

    Added Merge Sort Feature

---
+++ Algorithm/Algorithm/MergeSort.cs
@@ -0,0 +1,60 @@
+﻿namespace Algorithm
+{
+    internal class MergeSorts
+    {
+        static void Merge(int[] a, int beg, int mid, int end)
+        {
+            int i, j, k;
+            int n1 = mid - beg + 1;
+            int n2 = end - mid;
+            int[] leftArray = new int[n1];
+            int[] rightArray = new int[n2];
+            for (i = 0; i < n1; i++)
+                leftArray[i] = a[beg + i];
+            for (j = 0; j < n2; j++)
+                rightArray[j] = a[mid + 1 + j];
+
+            i = 0;
+            j = 0;
+            k = beg;
+
+            while (i < n1 && j < n2)
+            {
+                if (leftArray[i] <= rightArray[j])
+                {
+                    a[k] = leftArray[i];
+                    i++;
+                }
+                else
+                {
+                    a[k] = rightArray[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < n1)
+            {
+                a[k] = leftArray[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2)
+            {
+                a[k] = rightArray[j];
+                j++;
+                k++;
+            }
+        }
+        public void MergeSort(int[] a, int beg, int end)
+        {
+            if (beg < end)
+            {
+                int mid = (beg + end) / 2;
+                MergeSort(a, beg, mid);
+                MergeSort(a, mid + 1, end);
+                Merge(a, beg, mid, end);
+            }
+        }
+    }
+}
--- Algorithm/Algorithm/Program.cs
+++ Algorithm/Algorithm/Program.cs
@@ -8,66 +8,84 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose 1: For String Permutations");
-            Console.WriteLine("Choose 2: For Binary Search");
-            Console.WriteLine("Choose 3: For Insertion Sort");
-            Console.WriteLine("Choose 4: For Bubble Sort");
-            int options = int.Parse(Console.ReadLine());
-
-            switch (options)
+            int options = 0;
+            do
             {
-                case 1:
-                    Console.WriteLine("Enter a string");
-                    string word = Console.ReadLine();
-                    Console.WriteLine("Recursive ->");
-                    StringPermutation.RecursivePermutation(word, 0, word.Length - 1);
-                    Console.WriteLine("Iterarive ->");
-                    StringPermutation.IterativeFindPermutation("", word);
-                    break;
-                case 2:
-                    string sentence = "hello,coding,is,fun";
-                    Console.WriteLine($"The sentence is - {sentence}");
-                    string[] array = sentence.Split(",");
-                    BinarySearch<string> search = new BinarySearch<string>(array[0]);
-                    for (int i = 1; i < array.Length; i++)
-                    {
-                        search.Insert(array[i]);
-                    }
-                    Console.WriteLine("Enter word to display");
-                    string input = Console.ReadLine();
-                    bool found = search.Search(input, search);
-                    Console.WriteLine($"{input} found - " + found);
-                    break;
-                case 3:
-                    InsertionSort insertionSort = new InsertionSort();
-                    Console.WriteLine("Enter some Words: ");
-                    string words = Console.ReadLine();
-                    string[] wordsArray = words.Split(" ");
-                    insertionSort.InsertSort(wordsArray);
-                    foreach (var item in wordsArray)
-                    {
-                        Console.Write(item + " ");
-                    }
-                    break;
-                case 4:
-                    BubbleSort bubbleSort = new BubbleSort();
-                    List<int> numbers = new List<int>();
-                    for (int i = 0; i < 5; i++)
-                    {
-                        Console.WriteLine($"Enter {i + 1} Number: ");
-                        int num = int.Parse(Console.ReadLine());
-                        numbers.Add(num);
-                    }
-                    bubbleSort.BubbleSortNumber(numbers);
-                    for (int i = 0; i < numbers.Count; i++)
-                    {
-                        Console.WriteLine(numbers[i]);
-                    }
-                    break;
-                default:
-                    Console.WriteLine("Choose valid option");
-                    break;
-            }
+                Console.WriteLine("Choose 1: For String Permutations");
+                Console.WriteLine("Choose 2: For Binary Search");
+                Console.WriteLine("Choose 3: For Insertion Sort");
+                Console.WriteLine("Choose 4: For Bubble Sort");
+                Console.WriteLine("Choose 5: For Merge Sort");
+                Console.WriteLine("Choose 0: To Exit");
+                options = int.Parse(Console.ReadLine());
+
+                switch (options)
+                {
+                    case 1:
+                        Console.WriteLine("Enter a string");
+                        string word = Console.ReadLine();
+                        Console.WriteLine("Recursive ->");
+                        StringPermutation.RecursivePermutation(word, 0, word.Length - 1);
+                        Console.WriteLine("Iterarive ->");
+                        StringPermutation.IterativeFindPermutation("", word);
+                        break;
+                    case 2:
+                        string sentence = "hello,coding,is,fun";
+                        Console.WriteLine($"The sentence is - {sentence}");
+                        string[] array = sentence.Split(",");
+                        BinarySearch<string> search = new BinarySearch<string>(array[0]);
+                        for (int i = 1; i < array.Length; i++)
+                            search.Insert(array[i]);
+                        Console.WriteLine("Enter word to display");
+                        string input = Console.ReadLine();
+                        bool found = search.Search(input, search);
+                        Console.WriteLine($"{input} found - " + found);
+                        break;
+                    case 3:
+                        InsertionSort insertionSort = new InsertionSort();
+                        Console.WriteLine("Enter some Words: ");
+                        string words = Console.ReadLine();
+                        string[] wordsArray = words.Split(" ");
+                        insertionSort.InsertSort(wordsArray);
+                        foreach (var item in wordsArray)
+                            Console.Write(item + " ");
+                        break;
+                    case 4:
+                        BubbleSort bubbleSort = new BubbleSort();
+                        List<int> numbers = new List<int>();
+                        for (int i = 0; i < 5; i++)
+                        {
+                            Console.WriteLine($"Enter {i + 1} Number: ");
+                            int num = int.Parse(Console.ReadLine());
+                            numbers.Add(num);
+                        }
+                        bubbleSort.BubbleSortNumber(numbers);
+                        for (int i = 0; i < numbers.Count; i++)
+                            Console.WriteLine(numbers[i]);
+                        break;
+                    case 5:
+                        MergeSorts mergeSort = new MergeSorts();
+                        Console.WriteLine("Enter size of array: ");
+                        int size = int.Parse(Console.ReadLine());
+                        int[] arrayMergeSort = new int[size];
+                        for (int i = 0; i < arrayMergeSort.Length; i++)
+                        {
+                            Console.WriteLine($"Enter {i + 1} number: ");
+                            arrayMergeSort[i] = int.Parse(Console.ReadLine());
+                        }
+                        mergeSort.MergeSort(arrayMergeSort, 0, arrayMergeSort.Length - 1);
+                        foreach (var item in arrayMergeSort)
+                            Console.Write(item + " ");
+                        Console.WriteLine();
+                        break;
+                    case 0:
+                        Console.WriteLine("Exit");
+                        break;
+                    default:
+                        Console.WriteLine("Choose a valid option from above");
+                        break;
+                }
+            } while (options != 0);
         }
     }
 }
